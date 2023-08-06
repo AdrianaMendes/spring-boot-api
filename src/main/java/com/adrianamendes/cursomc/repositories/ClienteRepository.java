98d22116a888e12c60b377836e45956e87a9cbba
Adriana Cardoso <adrianamirianmc@gmail.com>
2023-08-06 13:31:21 -0300

    Cliente, TipoCliente, telefones e endereços

---
+++ src/main/java/com/adrianamendes/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.adrianamendes.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adrianamendes.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+} 
