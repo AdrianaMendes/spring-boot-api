d190743114a99aab2133f21e2a84c4321d9b36e1
Adriana Cardoso <adrianamirianmc@gmail.com>
2023-08-06 13:49:34 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/adrianamendes/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.adrianamendes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianamendes.cursomc.domain.Cliente;
+import com.adrianamendes.cursomc.repositories.ClienteRepository;
+import com.adrianamendes.cursomc.services.exceptions.ObjectNotFountException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		if(!obj.isPresent()) {
+			throw new ObjectNotFountException("Objeto não encontrado! Id: " + id + "Tipo: " + Cliente.class.getName());
+		}
+		return obj.get();
+	}
+}
