a262eb98435675def42b8f297cdeee502d9bb7a9
Adriana Cardoso <adrianamirianmc@gmail.com>
2023-08-10 17:03:56 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/adrianamendes/cursomc/services/CategoriaService.java
+++ src/main/java/com/adrianamendes/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.adrianamendes.cursomc.domain.Categoria;
+import com.adrianamendes.cursomc.dto.CategoriaDTO;
 import com.adrianamendes.cursomc.repositories.CategoriaRepository;
 import com.adrianamendes.cursomc.services.exceptions.DataIntegrityException;
 import com.adrianamendes.cursomc.services.exceptions.ObjectNotFountException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
